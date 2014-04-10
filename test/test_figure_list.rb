24a51edc4e4d69d09d5000fb5bb806c32ea0b7d8
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-09 22:20:33 -0400

    Call generate_figlist when testing

--- test/test_figure_list.rb
+++ test/test_figure_list.rb
@@ -4,6 +4,6 @@ require "figure_list"
 class FigureListTest < Test::Unit::TestCase
 	def test_file
 		assert_equal "# Figures\n\n![Jan Baptist Collaert, *Color Olivi*, c. 1590. Engraving, 521 x 414 mm.. Rijksmuseum, Amsterdam. (Image source: http://rijksmuseum.nl/)\\label{fig:collaert_1590}](/foo/bar.jpg)\n", 
-      FigureList.new("sample.yaml")
+      FigureList.new("test/sample.yaml").generate_figlist
   end
 end
