87816c718ef428a619b2799fe18a318bcabcd74a
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-09 23:20:30 -0400

    method for writing out to file

--- test/test_figure_list.rb
+++ test/test_figure_list.rb
@@ -6,4 +6,10 @@ class FigureListTest < Test::Unit::TestCase
 		assert_equal File.open("test/sample.md", "r").read, 
 	      FigureList.new("test/sample.yaml").generate_figlist
 	end
+
+	def test_output
+		out_path = "test/test.md"
+		FigureList.new("test/sample.yaml").figlist_to_file(out_path)
+		assert_equal File.open("test/sample.md", "r").read,
+			File.open("test/test.md", "r").read
 end
