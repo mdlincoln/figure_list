f9f18b06cbb28a1454504e6bc07e8b494ff72c54
mdlincoln <matthew.d.lincoln@gmail.com>
2014-04-09 22:25:32 -0400

    Read in assertion answer from file

---
+++ test/sample.md
@@ -0,0 +1,6 @@
+# Figures
+
+![Jan Baptist Collaert, *Color Olivi*, c. 1590. Engraving, 521 x 414 mm.. Rijksmuseum, Amsterdam. (Image source: http://rijksmuseum.nl/)\label{fig:collaert_1590}](/foo/bar.jpg)
+
+![Maarten van Heemskerck, *Saint Luke Painting the Madonna*, 1532. Oil on oak panel, 168 x 235 cm. Frans Hals Museum, Haarlem. (Image source: http://commons.wikimedia.org/)\label{fig:heemskerck_1532}](/foo/blerg.jpg)
+
--- test/test_figure_list.rb
+++ test/test_figure_list.rb
@@ -3,7 +3,7 @@ require "figure_list"
 
 class FigureListTest < Test::Unit::TestCase
 	def test_file
-		assert_equal "# Figures\n\n![Jan Baptist Collaert, *Color Olivi*, c. 1590. Engraving, 521 x 414 mm.. Rijksmuseum, Amsterdam. (Image source: http://rijksmuseum.nl/)\\label{fig:collaert_1590}](/foo/bar.jpg)\n", 
-      FigureList.new("test/sample.yaml").generate_figlist
-  end
+		assert_equal File.open("test/sample.md", "r").read, 
+	      FigureList.new("test/sample.yaml").generate_figlist
+	end
 end
