128403cb1bab0d262c6d5ab0440f396299e7e9b0
mdlincoln <matthew.d.lincoln@gmail.com>
2014-06-16 20:28:06 -0400

    Allow custom captions using "source" key

--- lib/figure_list/caption.rb
+++ lib/figure_list/caption.rb
@@ -1,20 +1,30 @@
 # Caption class that contains all the variables needed to render a caption
 class Caption
 	def initialize(label, figure)
-		@label = label
-		@artist = figure["artist"]
-    @title = figure["title"]
-    @date = figure["date"]
-    @medium = figure["medium"]
-    @dimensions = figure["dimensions"]
-    @collection = figure["collection"]
-    @location = figure["location"]
-    @source = figure["source"]
-    @img = figure["img"]
+    if figure.key?("custom")
+      @label = label
+      @custom = figure["custom"]
+      @img = figure["img"]
+    else
+  		@label = label
+  		@artist = figure["artist"]
+      @title = figure["title"]
+      @date = figure["date"]
+      @medium = figure["medium"]
+      @dimensions = figure["dimensions"]
+      @collection = figure["collection"]
+      @location = figure["location"]
+      @source = figure["source"]
+      @img = figure["img"]
+    end
   end
 
   # Render a caption in a mix of Markdown and LaTeX for processing by Pandoc
   def render
-    return "![#{@artist}, *#{@title}*, #{@date}. #{@medium.capitalize}, #{@dimensions}. #{@collection}, #{@location}. (Image source: #{@source})\\label{fig:#{@label}}](#{@img})"
+    if @custom.nil?
+      return "![#{@artist}, *#{@title}*, #{@date}. #{@medium.capitalize}, #{@dimensions}. #{@collection}, #{@location}. (Image source: #{@source})\\label{fig:#{@label}}](#{@img})"
+    else
+      return "![#{@custom}\\label{fig:#{@label}}](#{@img})"
+    end
   end
 end
